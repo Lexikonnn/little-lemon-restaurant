57a36fbee834201a7c7ba20106be8c45374a36ae
skrzeczek <h.skrzeczek@gmail.com>
2025-05-06 10:40:13 +0200

    added react helmet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Helmet>
+        <meta name="description" content="Best and cozy family restaurant in city, best prices." />
+        <meta property="og:title" content="Wonderful family Little Lemon restaurant" />
+        <meta property="og:description" content="Best and cozy family restaurant in city, best prices and service." />
+      </Helmet>
+    </>
   );
 }
 
