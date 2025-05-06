a76dfd3dc070d54eccf95d6b34a8e53a01eab846
skrzeczek <h.skrzeczek@gmail.com>
2025-05-06 11:15:10 +0200

    added routing and pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Helmet } from 'react-helmet';
+import { Routes, Route } from
 
 function App() {
   return (
@@ -8,6 +9,10 @@ function App() {
         <meta property="og:title" content="Wonderful family Little Lemon restaurant" />
         <meta property="og:description" content="Best and cozy family restaurant in city, best prices and service." />
       </Helmet>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/booking" element={<BookingPage />}></Route>
+      </Routes>
     </>
   );
 }
